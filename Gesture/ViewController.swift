fb2573d3e86289d0f7a60894668479044e57d5ad
maymayyim <mayyim.putti@gmail.com>
2017-11-02 12:04:59 +0700

    First commit

--- Gesture/ViewController.swift
+++ Gesture/ViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let images = ["1","2","3"]
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var currentImage = 0
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if(event?.subtype == UIEventSubtype.motionShake){
+            print("shake")
+            imageView.image = UIImage(named: images[currentImage]+".jpg")
+            if(currentImage == images.count-1){
+                currentImage = 0
+            }
+            else{
+                currentImage += 1
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
